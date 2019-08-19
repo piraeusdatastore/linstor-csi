695f20ba0a39105cde38cfbbf344f8944930aef3
Piotr Prokop <piotr.prokop@intel.com>
2019-08-19 11:17:41 +0200

    Rebase balancer to v0.7.2

--- pkg/topology/scheduler/balancer/balancer.go
+++ pkg/topology/scheduler/balancer/balancer.go
@@ -3,11 +3,14 @@ package balancer
 import (
 	"context"
 	"fmt"
-	"strings"
 
-	lc "github.com/LINBIT/golinstor"
+	golinstor "github.com/LINBIT/golinstor"
 	lapi "github.com/LINBIT/golinstor/client"
+	lc "github.com/LINBIT/linstor-csi/pkg/linstor/highlevelclient"
+	"github.com/LINBIT/linstor-csi/pkg/linstor/util"
+	"github.com/LINBIT/linstor-csi/pkg/topology"
 	"github.com/LINBIT/linstor-csi/pkg/volume"
+
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
@@ -24,11 +27,7 @@ const (
 	RackLabel      = "failure-domain.beta.kubernetes.io/zone"
 	StorageLabel   = "node-role.kubernetes.io/storage"
 	PrefNicPropKey = "PrefNic"
-	// LinstorNodeTopologyKey refers to a node running the LINSTOR csi node service
-	// and the linstor Satellite and is therefore capabile of hosting LINSTOR volumes.
-	// TODO: it should be move to utils
-	LinstorNodeTopologyKey = "linbit.com/hostname"
-	layerListKey           = "layerlist"
+	layerListKey   = "layerlist"
 )
 
 type StoragePool struct {
@@ -59,55 +58,6 @@ type BalanceDecision struct {
 
 type NodeLinstorClient interface {
 	GetStoragePools(ctx context.Context, nodeName string, opts ...*lapi.ListOpts) ([]lapi.StoragePool, error)
-}
-
-// TODO: move to utils
-type parameters struct {
-	layerList               []lapi.LayerType
-	allowRemoteVolumeAccess bool
-}
-
-// TODO: move to utils
-func parseLayerList(s string) ([]lapi.LayerType, error) {
-	list := strings.Split(s, " ")
-	var layers = make([]lapi.LayerType, 0)
-	knownLayers := []lapi.LayerType{lapi.DRBD, lapi.STORAGE, lapi.LUKS, lapi.NVME}
-
-userLayers:
-	for _, l := range list {
-		for _, k := range knownLayers {
-			if strings.EqualFold(l, string(k)) {
-				layers = append(layers, k)
-				continue userLayers
-			}
-		}
-		// Reached the bottom without finding a match.
-		return layers, fmt.Errorf("unknown layer type %s, known layer types %v", l, knownLayers)
-	}
-	return layers, nil
-}
-
-// TODO: move to utils
-// newParameters parses out the raw parameters we get and sets appropreate
-// zero values
-func newParameters(params map[string]string) (parameters, error) {
-	// set zero values
-	var p = parameters{
-		layerList: []lapi.LayerType{lapi.DRBD, lapi.STORAGE},
-	}
-
-	for k, v := range params {
-		switch strings.ToLower(k) {
-		case layerListKey:
-			l, err := parseLayerList(v)
-			if err != nil {
-				return p, err
-			}
-			p.layerList = l
-		}
-	}
-
-	return p, nil
 }
 
 func GetInternalk8sClient() (clientset kubernetes.Interface, err error) {
@@ -350,55 +300,41 @@ func pickStoragePool(ctx context.Context, nClient NodeLinstorClient, clientset k
 }
 
 type BalanceScheduler struct {
-	log       *logrus.Entry
-	client    *lapi.Client
+	log *logrus.Entry
+	*lc.HighLevelClient
 	clientset kubernetes.Interface
 }
 
-func NewBalanceScheduler(log *logrus.Entry, lClient *lapi.Client) (b BalanceScheduler, err error) {
+func NewScheduler(c *lc.HighLevelClient, log *logrus.Entry) (b BalanceScheduler, err error) {
 	clientset, err := k8sClient()
 	if err != nil {
 		return b, err
 	}
 
 	return BalanceScheduler{
-		log:       log,
-		client:    lClient,
-		clientset: clientset,
+		log:             log,
+		HighLevelClient: c,
+		clientset:       clientset,
 	}, nil
 }
 
-func (b BalanceScheduler) deploy(ctx context.Context, vol *volume.Info, params parameters, node string, storagePool string) error {
-	return b.client.Resources.Create(ctx, volToDiskfullResourceCreate(vol, params, node, storagePool))
+func (b BalanceScheduler) deploy(ctx context.Context, vol *volume.Info, params volume.Parameters, node string, storagePool string) error {
+	return b.Resources.Create(ctx, volToDiskfullResourceCreate(vol, params, node, storagePool))
 }
 
 func (b BalanceScheduler) Create(ctx context.Context, vol *volume.Info, req *csi.CreateVolumeRequest) error {
-	b.log.WithFields(logrus.Fields{
-		"volume": fmt.Sprintf("%+v", vol),
-	}).Info("creating volume")
+	topos := req.GetAccessibilityRequirements()
+	if topos == nil {
+		return fmt.Errorf("no volume topologies, unable to schedule volume %s", vol.ID)
+	}
 
-	params, err := newParameters(vol.Parameters)
+	params, err := volume.NewParameters(vol.Parameters)
 	if err != nil {
 		return fmt.Errorf("unable to create volume due to bad parameters %+v: %v", vol.Parameters, err)
 	}
 
-	if !params.allowRemoteVolumeAccess {
+	if !params.AllowRemoteVolumeAccess {
 		return fmt.Errorf("placementPolicyBalance cannot work on on local storage")
-	}
-
-	// If we don't care about local storage or nodes are being manually assigned
-	// or there are no topology preferences skip the topology based assignment logic.
-	topos := req.GetAccessibilityRequirements()
-	if topos == nil {
-		decision, err := pickStoragePool(ctx, b.client.Nodes, b.clientset)
-		if err != nil {
-			return err
-		}
-
-		// deploy to Node and StoragePool picked by Balancer
-		if err := b.deploy(ctx, vol, params, decision.NodeName, decision.StoragePoolName); err != nil {
-			return fmt.Errorf("cannot deploy volume: %s on Node: %s, and StoragePool: %s: %s", vol.ID, decision.NodeName, decision.StoragePoolName, err)
-		}
 	}
 
 	// For now we do not support more than one Diskfull Resources so set remainingAssignments to 1
@@ -407,9 +343,9 @@ func (b BalanceScheduler) Create(ctx context.Context, vol *volume.Info, req *csi
 	for i, pref := range topos.GetPreferred() {
 		// While there are still preferred nodes and remainingAssignments
 		// attach resources diskfully to those nodes in order of most to least preferred.
-		if p, ok := pref.GetSegments()[LinstorNodeTopologyKey]; ok && remainingAssignments > 0 {
+		if p, ok := pref.GetSegments()[topology.LinstorNodeKey]; ok && remainingAssignments > 0 {
 			// If attachment fails move onto next most preferred volume.
-			decision, err := pickStoragePoolTopo(ctx, p, b.client.Nodes, b.clientset)
+			decision, err := pickStoragePoolTopo(ctx, p, b.Nodes, b.clientset)
 			if err != nil {
 				b.log.WithFields(logrus.Fields{
 					"volumeID":                   vol.ID,
@@ -450,85 +386,35 @@ func (b BalanceScheduler) Create(ctx context.Context, vol *volume.Info, req *csi
 }
 
 func (b BalanceScheduler) AccessibleTopologies(ctx context.Context, vol *volume.Info) ([]*csi.Topology, error) {
-	p, err := newParameters(vol.Parameters)
+	r, err := b.Resources.GetAll(ctx, vol.ID)
 	if err != nil {
 		return nil, fmt.Errorf("unable to determine AccessibleTopologies: %v", err)
 	}
-
-	if p.allowRemoteVolumeAccess {
-		r, err := b.client.Resources.GetAll(ctx, vol.ID)
-		if err != nil {
-			return nil, fmt.Errorf("unable to determine AccessibleTopologies: %v", err)
-		}
-		nodes := deployedNodes(r)
-		// all nodes will be in the same Rack so take only 1 of them
-		rack, err := retrieveRackId(b.clientset, nodes[0])
-		if err != nil {
-			return nil, err
-		}
-		var topos = make([]*csi.Topology, 0)
-		topos = append(topos, &csi.Topology{Segments: map[string]string{RackLabel: rack}})
-		return topos, nil
+	nodes := util.DeployedDiskfullyNodes(r)
+	if len(nodes) == 0 {
+		return nil, fmt.Errorf("volume %s has no diskfull resource", vol.ID)
 	}
-
-	return nil, nil
+	// all nodes will be in the same Rack so take only 1 of them
+	rack, err := retrieveRackId(b.clientset, nodes[0])
+	if err != nil {
+		return nil, err
+	}
+	var topos = make([]*csi.Topology, 0)
+	topos = append(topos, &csi.Topology{Segments: map[string]string{RackLabel: rack}})
+	return topos, nil
 }
 
-// TODO: it should be moved to util package
-func volToDiskfullResourceCreate(vol *volume.Info, params parameters, node string, storagePool string) lapi.ResourceCreate {
-	res := volToGenericResourceCreate(vol, params, node)
-	res.Resource.Props[lc.KeyStorPoolName] = storagePool
-	return res
-}
-
-// TODO: it should be moved to util package
-func volToGenericResourceCreate(vol *volume.Info, params parameters, node string) lapi.ResourceCreate {
+func volToDiskfullResourceCreate(vol *volume.Info, params volume.Parameters, node string, storagePool string) lapi.ResourceCreate {
 	return lapi.ResourceCreate{
-		LayerList: params.layerList,
+		LayerList: params.LayerList,
 		Resource: lapi.Resource{
 			Name:     vol.ID,
 			NodeName: node,
-			Props:    make(map[string]string, 1),
-			Flags:    make([]string, 0),
-		}}
-}
-
-// TODO: move to utils
-func containsAll(list []string, candidates ...string) bool {
-	if len(candidates) == 0 {
-		return false
+			Props: map[string]string{
+				golinstor.KeyStorPoolName: storagePool,
+			},
+			Flags: make([]string, 0),
+		},
 	}
 
-nextCandidate:
-	for _, c := range candidates {
-		for _, e := range list {
-			if e == c {
-				continue nextCandidate
-			}
-		}
-		// Made it through all data with no match.
-		return false
-	}
-	return true
-}
-
-// TODO: move to utils
-func doesNotcontainAll(list []string, candidates ...string) bool {
-	return !containsAll(list, candidates...)
-}
-
-// TODO: move to utils
-func deployed(res lapi.Resource) bool {
-	return doesNotcontainAll(res.Flags, lc.FlagDiskless)
-}
-
-// TODO: move to utils
-func deployedNodes(res []lapi.Resource) []string {
-	var nodes = make([]string, 0)
-	for _, r := range res {
-		if deployed(r) {
-			nodes = append(nodes, r.NodeName)
-		}
-	}
-	return nodes
 }
