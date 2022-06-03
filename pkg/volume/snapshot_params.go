d17f909a7ece5cfca8ea51f85a3bc17bbaf29a10
Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>
2022-06-03 09:03:29 +0200

    update golinstor
    * golinstor 0.42.0
    * update mocks
    * update go generate calls to all use go 1.17 run syntax
    
    Signed-off-by: Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>

--- pkg/volume/snapshot_params.go
+++ pkg/volume/snapshot_params.go
@@ -10,7 +10,7 @@ import (
 	"github.com/piraeusdatastore/linstor-csi/pkg/linstor"
 )
 
-//go:generate enumer -type=SnapshotType -trimprefix=SnapshotType
+//go:generate go run github.com/alvaroloes/enumer@v1.1.2 -type=SnapshotType -trimprefix=SnapshotType
 
 type SnapshotType int
 
