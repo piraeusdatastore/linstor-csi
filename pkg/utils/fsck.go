bb36a6896e026b6f17481f16c47340e62f2cb073
Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>
2025-11-04 10:56:52 +0100

    run fsck before mounting
    We had repeated issues with ext4 in particular, as that expects to run e2fsck
    from time to time. Why a journaling filesystem is so paranoid remains unknown,
    but we can try to be nice and run fsck.
    
    Signed-off-by: Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>

---
+++ pkg/utils/fsck.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os/exec"
+)
+
+// Fsck runs fsck on an unmounted device in "automatic" mode.
+//
+// The device is expected to contain a valid xfs or ext4 filesystem.
+func Fsck(ctx context.Context, device string) error {
+	// -l: lock the devices in /run/fsck/<device>.lock
+	// -p: run into "auto" mode, not requesting confirmation on TTY.
+	//     This is an option that is specific to fsck.ext4 and fsck.xfs, passed along by fsck.
+	out, err := exec.CommandContext(ctx, "fsck", "-l", "-p", device).CombinedOutput()
+	if err != nil {
+		var execErr *exec.ExitError
+		if errors.As(err, &execErr) && execErr.ExitCode() <= 1 {
+			// Exit code 0: No error
+			// Exit code 1: Filesystem errors corrected
+			return nil
+		}
+
+		return fmt.Errorf("failed to run fsck: output: \"%s\", %w", out, err)
+	}
+
+	return nil
+}
