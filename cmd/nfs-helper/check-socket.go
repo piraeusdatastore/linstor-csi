d50941d03dc1db054dfff5d0bc7ab55a6d627e58
Moritz Tanner <moritz.wanzenboeck@linbit.com>
2026-05-06 12:23:54 +0200

    nfs: verify ganesha NFS socket is ready after service start
    Add a check-socket subcommand to nfs-helper that polls localhost:<port>
    until ganesha accepts TCP connections, ensuring the service is only
    considered started once the socket is actually available.
    
    Signed-off-by: Moritz Tanner <moritz.wanzenboeck@linbit.com>

---
+++ cmd/nfs-helper/check-socket.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func checkSocket(ctx context.Context, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("expected one argument: <resource-name>")
+	}
+
+	resource := args[0]
+
+	config, err := readMetadata(resource)
+	if err != nil {
+		return fmt.Errorf("failed to read resource metadata: %w", err)
+	}
+
+	addr := fmt.Sprintf("localhost:%d", config.Port)
+
+	log.WithField("addr", addr).Info("Waiting for NFS socket")
+
+	for {
+		conn, err := net.DialTimeout("tcp", addr, time.Second)
+		if err == nil {
+			conn.Close()
+			log.WithField("addr", addr).Info("NFS socket is ready")
+
+			return nil
+		}
+
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("timed out waiting for NFS socket on %s: %w", addr, ctx.Err())
+		case <-time.After(time.Second):
+		}
+	}
+}
